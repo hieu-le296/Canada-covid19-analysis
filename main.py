b84c62bf50e04b2a7c13f3ea6a27df75bdce58a8
Hieu Le <hieu.le@student.ufv.ca>
2020-12-08 01:51:45 -0800

    Create Data class to extract data from API

--- main.py
+++ main.py
@@ -2,24 +2,37 @@ import requests
 import json
 import csv
 
-r = requests.get('https://api.covid19tracker.ca/reports')
+class Data:
+    def __init__(self, *args, **kwargs):
+        request_canada = requests.get('https://api.covid19tracker.ca/reports')
+        request_bc = requests.get('https://api.covid19tracker.ca/reports/province/bc')
 
-covid_data = json.loads(r.text)
-covid_list = covid_data['data']
+        canada_covid_data = json.loads(request_canada.text)
+        canada_covid_list = canada_covid_data['data']
 
-with open('bc_daily_cases.csv', 'w') as csv_file:
-    csv_writer = csv.writer(csv_file)
+        bc_covid_data = json.loads(request_bc.text)
+        bc_covid_list = bc_covid_data['data']
 
-    for item in covid_list:
-        date = item['date']
-        active_cases = item['change_cases']
-        new_deaths = item['change_fatalities']
-        new_recoveries = item['change_recoveries']
-        total_cases = item['total_cases']
-        total_death = item['total_fatalities']
-        total_recoveries = item['total_recoveries']
-        line = [date, active_cases, new_deaths, new_recoveries, total_cases, total_death, total_recoveries]
-        print(line)
-        csv_writer.writerow(line)
+        self.get_canada_cases(canada_covid_list)
+        self.get_bc_cases(bc_covid_list)
+
+    def get_canada_cases(self, canada_covid_list):
+        with open('canada_cases.csv', 'w') as csv_file:
+            self.write_to_csv(csv_file, canada_covid_list)
     
-
+    def get_bc_cases(self, bc_covid_list):
+        with open('bc_cases.csv', 'w') as csv_file:
+            self.write_to_csv(csv_file, bc_covid_list)
+    
+    def write_to_csv(self, csv_file, covid_list):
+        csv_writer = csv.writer(csv_file)
+        for item in covid_list:
+            date = item['date']
+            active_cases = item['change_cases']
+            new_deaths = item['change_fatalities']
+            new_recoveries = item['change_recoveries']
+            total_cases = item['total_cases']
+            total_death = item['total_fatalities']
+            total_recoveries = item['total_recoveries']
+            line = [date, active_cases, new_deaths, new_recoveries, total_cases, total_death, total_recoveries]
+            csv_writer.writerow(line)
